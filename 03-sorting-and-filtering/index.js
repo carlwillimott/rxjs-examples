7eebf956697f43503806406cc89431dc39563a97
carlwillimott <carlwillimott@googlemail.com>
2016-06-04 10:55:46 +0100

    Added the country flags.

--- 03-sorting-and-filtering/index.js
+++ 03-sorting-and-filtering/index.js
@@ -1,6 +1,16 @@
 const filter = document.querySelector('#filter');
 const order = document.querySelector('#order');
 
+const = countries = [
+  {
+    country: "United Kingdom",
+    continent: "europe",
+    flag: "flags/1465051433_United-Kingdom_flat.png",
+    capital: "London",
+    population: 64100000
+  }
+];
+
 const filter$ = Rx.Observable.fromEvent(filter, 'change')
   .map((x)=> x.target.value)
   .subscribe((x)=> console.log(x));
@@ -9,3 +19,7 @@ const order$ = Rx.Observable.fromEvent(order, 'change')
   .map((x)=> x.target.value)
   .startWith('a-z')
   .subscribe((x)=> console.log(x));
+
+const countries$ = Rx.Observable.of(countries);
+
+const countriesFilterOrder$ = null;
