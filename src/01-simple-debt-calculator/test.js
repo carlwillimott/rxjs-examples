f256d61fbaabb697c4d6cf2a8e91a431b43f93c9
carlwillimott <carlwillimott@googlemail.com>
2016-06-27 19:54:31 +0100

    Managed to get an initial unit test working.

--- src/01-simple-debt-calculator/test.js
+++ src/01-simple-debt-calculator/test.js
@@ -1,11 +1,17 @@
-'use strict';
-
-require('./index.js');
+import {calculateDebt} from './helpers';
 
 describe("SimpleDebtCalculator", function() {
 
 	it("should be able to calculate the debt", function() {
 		expect(calculateDebt(1000, 5, 3)).toEqual(1150);
+		expect(calculateDebt(500, 6, 3)).toEqual(590);
+		expect(calculateDebt(0, 0, 10)).toEqual(0);
+	});
+
+	it("should be able to handle negative numbers", function() {
+		expect(calculateDebt(-3, 2, 4)).toEqual(-3.24);
+		expect(calculateDebt(-3, -5, -10)).toEqual(-4.5);
+		expect(calculateDebt(500, 0.5, 25)).toEqual(562.5);
 	});
 
 });
